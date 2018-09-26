a5d038a4a8b0bac826ef0953195902ade62fe934
NAUMAN\STARIZ.PK <naumanmoazzam3@gmail.com>
2018-09-26 19:36:29 +0500

    React Application with Mosh Tutorial

---
+++ src/components/counter.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  ComponentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+    }
+  }
+  ComponentWillUnmount() {
+    console.log("Counter - Unmount");
+  }
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // };
+  render() {
+    console.log("Counter - Rendered");
+
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes +=
+      this.props.counter.value === 0
+        ? "warning"
+        : this.props.counter.value < 0
+          ? "success"
+          : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value < 0 ? "Minus" : value;
+  }
+}
+
+export default Counter;
